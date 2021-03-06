f8b55bdc06e9864c38c3cfc88aee1674310fde6e
fabgth <fabiengauthey@gmail.com>
2021-03-06 23:47:49 +0100

    commit 1 test

---
+++ transformer.py
@@ -0,0 +1,17 @@
+import os
+import img2pdf
+
+with open("output.pdf","wb") as f:
+    f.write(img2pdf.convert([i for i in os.listdir('.') if i.endswith(".jpg") or i.endswith(".png")]))
+
+
+
+
+
+
+
+
+
+
+
+
